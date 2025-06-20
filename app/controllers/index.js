eebf800972b2413b3e2c70c51fe181cc05f51959
Sayan Shankhari <sayan0shankhari@gmail.com>
2025-06-20 14:35:37 +0530

    refactor (config): remove response ambiguity
    Able to get correct response.
    
    Remove ambiguous responses generated from multiple sources.
    Instead create single response object and pass around the
    responce object to controllers to fill the properties up.
    
    Resolves: #2

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,26 +5,22 @@ const version_handler = require ("./version.js");
 
 
 async function handle_request (request) {
-	let response = {};
-
 	switch (request .handler) {
 		case "general":
-			response = general_handler. handle_request (request .inputs);
+			data = general_handler. handle_request (request .inputs);
 			break;
 		case "version":
-			response = await version_handler. handle_request (request .inputs);
+			data = await version_handler. handle_request (request .inputs);
 			break;
 		default:
-			response .status = 200;
-			response .type = "text/html";
-			response .data = `<body>
+			data = `<body>
 <h2>Please use the following paramerets after default URL</h2>
 <h3>?type=version&user={Your GitHub Username}&acc={User or Organization Name}&repo={Repository Name}&branch={Branch Name}</h3>
 </body>`;
 			break;
 	}
 
-	return response;
+	return data;
 }
 
 
